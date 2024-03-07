0b51a2a9e1941f51629cc4af04cefcdfd6c7dcc5
YouriProgrammer <97059905+YOURIxYOURI@users.noreply.github.com>
2024-03-07 11:07:07 +0100

    add files

---
+++ 00000000001/desktop/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string pass;
+        static Random random = new Random();
+
+        private void Password_Gen_Button_Click(object sender, RoutedEventArgs e)
+        {
+            pass = "";
+            int.TryParse(PassLength.Text, out int passLength);
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            string numbers = "0123456789";
+            string signs = "!@#$%^&*()_+-=";
+            string allChars = "";
+            if ((bool)LettersCheck.IsChecked && pass.Length < passLength) 
+            {
+                int num = random.Next(0, letters.Length);
+                pass += letters[num];
+                allChars += letters;
+            }
+            if ((bool)NumbersCheck.IsChecked && pass.Length < passLength)
+            {
+                int num = random.Next(0, numbers.Length);
+                pass += numbers[num];
+                allChars += numbers;
+            }
+            if ((bool)SignsCheck.IsChecked && pass.Length < passLength)
+            {
+                int num = random.Next(0, signs.Length);
+                pass += signs[num];
+                allChars += signs;
+            }
+            while (pass.Length < passLength)
+            {
+                int num = random.Next(0, allChars.Length);
+                pass += allChars[num];
+            }
+            MessageBox.Show(pass);
+        }
+        private void Accept_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Dane pracownika: {FirstName.Text} {LastName.Text} {PositionBox.Text} {pass}");
+        }
+    }
+}
---
+++ 00000000001/konsola/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/**********************************************
+nazwa funkcji: < Find_NWD >
+opis funkcji: < funckja korzysta z algorytmu Euklidesa do wyszukiwania NWD>
+parametry: < a – Jedna z liczb potrzebnyc do algorymu
+    b – Jedna z liczb potrzebnyc do algorymu>
+zwracany typ i opis: < int - liczba odpadowiadająca NWD>
+autor: < 00000000001 >
+***********************************************
+*/
+int Find_NWD(int a, int b)
+{
+    while(a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+    return a;
+}
+Console.WriteLine("Podaj liczbe A");
+int user_a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbe B");
+int user_b = int.Parse(Console.ReadLine());
+if (user_a > 0 && user_b > 0)
+{
+    Console.WriteLine($"NWD podanych liczb {Find_NWD(user_a, user_b)}");
+}
+else
+{
+    Console.WriteLine("Liczby powinny być dodatnie");
+}
+
